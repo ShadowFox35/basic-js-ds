696a7656ef340f42f84fc1863e0cb800ee32c590
ShadowFox35 <94312514+ShadowFox35@users.noreply.github.com>
2022-05-02 21:46:39 +0300

    init: solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError,
+} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -15,18 +17,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
- */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  class ListNode {
+    constructor(x) {
+      this.value = x;
+      this.next = null;
+    }
+  }
+**/
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
+function removeKFromList(l, k) {
+  console.log(l);
+  let result;
+  let arr = l;
+  let count = 1;
+  let deep = 0;
+
+  while (arr.next) {
+    count += 1;
+    arr = arr.next;
+  }
+
+  while (count !== deep) {
+    if (!result && obj(l, deep).value !== k) {
+      result = new ListNode(obj(l, deep).value);
+    } else if (result && obj(l, deep).value !== k) {
+      last(result).next = new ListNode(obj(l, deep).value);
+    }
+    deep++;
+  }
+  
+  return result;
 }
 
+function obj(l, deep) {
+  if (deep == 0) {
+    return l;
+  }
+
+  return obj(l.next, deep - 1);
+}
+
+function last(result) {
+  if (!result.next) {
+    return result;
+  }
+
+  return last(result.next);
+}
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+
+const initial = convertArrayToList([3, 1, 2, 3, 4, 5]);
+const expected = convertArrayToList([1, 2, 4, 5]);
+console.log(removeKFromList(initial, 3));
+
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
